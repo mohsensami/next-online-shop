6fb58ef6facd0d9322e659eb166407eecd9d7ac8
MohsenSami <mohsensami88@gmail.com>
2023-03-24 13:50:28 +0330

    first layout

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Layout from "./components/Layout/Layout";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Layout>
+                    <p>Shopping App</p>
+                </Layout>
+            </div>
+        );
+    }
+}
+export default App;
