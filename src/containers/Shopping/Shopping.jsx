61bddbb24ca4d95954c3df3e89e9c9d3628536f0
MohsenSami <mohsensami88@gmail.com>
2023-03-29 13:02:29 +0330

    sending post request to firebase

--- src/containers/Shopping/Shopping.jsx
+++ src/containers/Shopping/Shopping.jsx
@@ -1,87 +1,93 @@
-import React from 'react'
+import React from 'react';
 
-import Wrapper from '../../hoc/Wrapper'
-import Controls from '../../components/Controls/Controls'
-import Modal from '../../components/UI/Modal/Modal'
-import Order from '../../components/Order/Order'
+import Wrapper from '../../hoc/Wrapper';
+import Controls from '../../components/Controls/Controls';
+import Modal from '../../components/UI/Modal/Modal';
+import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
 
 const prices = {
-  product1: 59,
-  product2: 89,
-  product3: 99,
-  product4: 79,
-}
+    product1: 59,
+    product2: 89,
+    product3: 99,
+    product4: 79,
+};
 
 class Shopping extends React.Component {
-  state = {
-    products: {
-      product1: 0,
-      product2: 0,
-      product3: 0,
-      product4: 0,
-    },
-    totalPrice: 0,
-    purchased: false,
-  }
+    state = {
+        products: {
+            product1: 0,
+            product2: 0,
+            product3: 0,
+            product4: 0,
+        },
+        totalPrice: 0,
+        purchased: false,
+    };
 
-  addProductHandler = (type) => {
-    const prevCount = this.state.products[type]
-    const updatedCount = prevCount + 1
-    const updatedProducts = {
-      ...this.state.products,
+    addProductHandler = (type) => {
+        const prevCount = this.state.products[type];
+        const updatedCount = prevCount + 1;
+        const updatedProducts = {
+            ...this.state.products,
+        };
+        updatedProducts[type] = updatedCount;
+        const priceAdd = prices[type];
+        const prevPrice = this.state.totalPrice;
+        const newPrice = prevPrice + priceAdd;
+        this.setState({ totalPrice: newPrice, products: updatedProducts });
+    };
+
+    removeProductHandler = (type) => {
+        const prevCount = this.state.products[type];
+        const updatedCount = prevCount - 1;
+        const updatedProducts = {
+            ...this.state.products,
+        };
+        updatedProducts[type] = updatedCount;
+        const priceSub = prices[type];
+        const prevPrice = this.state.totalPrice;
+        const newPrice = prevPrice - priceSub;
+        this.setState({ totalPrice: newPrice, products: updatedProducts });
+    };
+
+    purchasedHandler = () => {
+        this.setState({ purchased: true });
+    };
+
+    modalCloseHandler = () => {
+        this.setState({ purchased: false });
+    };
+
+    purchaseContinueHandler = () => {
+        const data = {
+            products: this.state.products,
+            totalprice: this.state.totalPrice,
+            customer: {
+                name: 'mohsen',
+                email: 'mohsen@gmail.com',
+            },
+        };
+        axios
+            .post('/orders.json', data)
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    render() {
+        return (
+            <Wrapper>
+                <Modal show={this.state.purchased} modalClose={this.modalCloseHandler}>
+                    <Order products={this.state.products} price={this.state.totalPrice} continue={this.purchaseContinueHandler} cancel={this.modalCloseHandler} />
+                </Modal>
+                <Controls productAdd={this.addProductHandler} productRemove={this.removeProductHandler} price={this.state.totalPrice} order={this.purchasedHandler} />
+            </Wrapper>
+        );
     }
-    updatedProducts[type] = updatedCount
-    const priceAdd = prices[type]
-    const prevPrice = this.state.totalPrice
-    const newPrice = prevPrice + priceAdd
-    this.setState({ totalPrice: newPrice, products: updatedProducts })
-  }
-
-  removeProductHandler = (type) => {
-    const prevCount = this.state.products[type]
-    const updatedCount = prevCount - 1
-    const updatedProducts = {
-      ...this.state.products,
-    }
-    updatedProducts[type] = updatedCount
-    const priceSub = prices[type]
-    const prevPrice = this.state.totalPrice
-    const newPrice = prevPrice - priceSub
-    this.setState({ totalPrice: newPrice, products: updatedProducts })
-  }
-
-  purchasedHandler = () => {
-    this.setState({ purchased: true })
-  }
-
-  modalCloseHandler = () => {
-    this.setState({ purchased: false })
-  }
-
-  purchaseContinueHandler = () => {
-    console.log('purchaseContinueHandler')
-  }
-
-  render() {
-    return (
-      <Wrapper>
-        <Modal show={this.state.purchased} modalClose={this.modalCloseHandler}>
-          <Order
-            products={this.state.products}
-            price={this.state.totalPrice}
-            continue={this.purchaseContinueHandler}
-            cancel={this.modalCloseHandler}
-          />
-        </Modal>
-        <Controls
-          productAdd={this.addProductHandler}
-          productRemove={this.removeProductHandler}
-          price={this.state.totalPrice}
-          order={this.purchasedHandler}
-        />
-      </Wrapper>
-    )
-  }
 }
 
-export default Shopping
+export default Shopping;
