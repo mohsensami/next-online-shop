ae5e4f32f4743c366e7c35b336cc44e53203a55d
mohsensami <mohsensami18727@gmail.com>
2024-04-06 22:56:52 +0330

    show Slider in Home page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
 import Image from 'next/image';
 import Header from './_components/Header';
+import Slider from './_components/Slider';
+import GlobalApi from './_utils/GlobalApi';
 
-export default function Home() {
+export default async function Home() {
+    const sliderList = await GlobalApi.getSliders();
     return (
         <>
             <Header />
+            {/* Sliders  */}
+            <div className="p-10 px-5 md:px-16">
+                <Slider sliderList={sliderList} />
+            </div>
         </>
     );
 }
